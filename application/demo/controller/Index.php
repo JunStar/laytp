b19fa290200074a6980bf8f78f98d3db4c324c1c
junstar <qmsjuncd@163.com>
2018-08-14 22:51:52 +0800

    模板demo

--- application/demo/controller/Index.php
+++ application/demo/controller/Index.php
@@ -5,7 +5,20 @@ use controller\BasicAdmin;
 
 class Index extends BasicAdmin
 {
+    //首页
     public function index()
+    {
+        return $this->fetch();
+    }
+
+    //表格
+    public function table()
+    {
+        return $this->fetch();
+    }
+
+    //表单
+    public function form()
     {
         return $this->fetch();
     }
