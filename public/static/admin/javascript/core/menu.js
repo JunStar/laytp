6851b9ffd08dbf2e82ef909110030c8d0b9bf829
junstar <qmsjuncd@163.com>
2018-08-22 21:45:06 +0800

    菜单管理

--- public/static/admin/javascript/core/menu.js
+++ public/static/admin/javascript/core/menu.js
@@ -10,16 +10,27 @@
             , cellMinWidth: 80 //全局定义常规单元格的最小宽度，layui 2.2.1 新增
             , page: true //开启分页
             , cols: [[ //表头
-                {field: 'id', title: 'ID', sort: true, fixed: 'left'}
-                , {field: 'name', title: '标题'}
-                , {field: 'rule', title: '规则', sort: true}
-                , {field: 'icon', title: '图标'}
-                , {field: 'sort', title: '排序'}
-                , {field: 'operation', title: '操作', toolbar:'#operation'}
+                {field: 'id', title: 'ID', sort: true, fixed: 'left', align: 'center'}
+                , {field: 'name', title: '标题', align: 'center'}
+                , {field: 'rule', title: '规则', sort: true, align: 'center'}
+                , {field: 'icon', title: '图标', align: 'center'}
+                , {field: 'sort', title: '排序', align: 'center'}
+                , {field: 'operation', title: '操作', toolbar:'#operation', fixed: 'right', align: 'center'}
             ]]
         });
 
-        table.on()
+        //监听工具条
+        table.on('tool(default)', function(obj){
+            var data = obj.data;
+            if(obj.event === 'del'){
+                layer.confirm('真的删除行么', function(index){
+                    obj.del();
+                    layer.close(index);
+                });
+            }else if(obj.event === 'edit'){
+                layer.alert('编辑行：<br>'+ JSON.stringify(data));
+            }
+        });
     }
 
     controller.table_render();
