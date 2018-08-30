67ffd0ad22a65e58ea855d0ec0e986fad010c3be
junstar <qmsjuncd@163.com>
2018-08-30 18:59:05 +0800

    CURD

--- public/static/admin/javascript/autocreate/curd/set_fields.js
+++ public/static/admin/javascript/autocreate/curd/set_fields.js
@@ -1,14 +1,14 @@
 (function func_controller() {
     var func_controller = {};
 
-    func_controller.table_render = function (where) {
+    func_controller.table_render = function () {
         table.render({
             elem: '.layui-hide-sm'
             ,url:''
             ,cellMinWidth: 100
             ,cols: [
                 [
-                    {title:'数据库字段设置', width:100, align: 'center', colspan: 8}
+                    {title:'数据库字段设置', width:100, align: 'center', colspan: 9}
                     ,{title:'表单设置,影响添加编辑表单', align: 'center', colspan: 2}
                     ,{title:'列表设置', align: 'center', colspan: 5}
                 ]
@@ -18,10 +18,11 @@
                     ,{title:'序号', type:'numbers', align: 'center'}
                     ,{field:'field_name', title:'字段名称', width:100, align: 'center', edit: 'text'}
                     ,{field:'field_comment', title:'字段注释(同时用于显示在表头和表单中的文字)', width:100, align: 'center', edit: 'text'}
-                    ,{field:'field_type', title:'字段类型', templet: '#field_type', align: 'center', width: 120}
-                    ,{field:'field_length', title:'字段长度', align: 'center', edit: 'text'}
-                    ,{field:'field_additional', title:'附加选项', templet: "#field_additional", align: 'center', width: 280}
-                    ,{field:'field_default_val', title:'默认值', align: 'center', edit: 'text'}
+                    ,{field:'field_type', title:'字段类型', templet: '#field_type', align: 'center', width: 190}
+                    ,{field:'field_length', title:'长度', align: 'center', edit: 'text'}
+                    ,{field:'field_decimal', title:'小数点', align: 'center', edit: 'text'}
+                    ,{field:'field_additional', title:'附加选项', templet: "#field_additional", align: 'center', width: 370}
+                    ,{field:'field_val', title:'值', toolbar: '#field_val', align: 'center', edit: 'text', width: 160}
                     //表单设置
                     ,{field:'form_type', title:'表单元素', templet: "#form_type", align: 'center', width: 280}
                     ,{field:'form_required', title:'附加选项', templet: "#form_additional", align: 'center', width: 120}
@@ -39,12 +40,22 @@
                     ,field_comment:"ID"
                     ,field_type:"int"
                     ,field_length:"11"
-                    ,field_default_val:""
+                    ,field_decimal:"0"
+                    ,field_val:""
                     ,table_width:"100"
                     ,table_min_width:"60"
                     ,table_type:"60"
                 }
             ]
+        });
+
+        form.on('select(field_type)',function(data){
+            console.log(data);
+            var sel_val = data.value;
+            //有int附加选项才有自增,主键,无符号,必填
+
+            // func_controller.table_render();
+            return true;
         });
     }
 
