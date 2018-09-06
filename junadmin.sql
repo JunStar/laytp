bcc83137e4f2aaa71833c6d9344e1c7feca0fa6a
junstar <qmsjuncd@163.com>
2018-09-06 22:42:31 +0800

    junadmin.sql

--- junadmin.sql
+++ junadmin.sql
@@ -1,56 +1,73 @@
--- MySQL dump 10.13  Distrib 5.7.23, for Linux (x86_64)
---
--- Host: localhost    Database: junadmin
--- ------------------------------------------------------
--- Server version	5.7.23-0ubuntu0.18.04.1
+/*
+ Navicat Premium Data Transfer
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50723
+ Source Host           : localhost:3306
+ Source Schema         : junadmin
 
---
--- Table structure for table `sys_menu`
---
+ Target Server Type    : MySQL
+ Target Server Version : 50723
+ File Encoding         : 65001
 
+ Date: 06/09/2018 22:40:33
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for sys_curd
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_curd`;
+CREATE TABLE `sys_curd`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '表名',
+  `comment` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '表注释',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_curd
+-- ----------------------------
+INSERT INTO `sys_curd` VALUES (1, 'ja_test', '测试模型');
+
+-- ----------------------------
+-- Table structure for sys_curd_model
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_curd_model`;
+CREATE TABLE `sys_curd_model`  (
+  `id` int(11) NOT NULL COMMENT 'ID',
+  `curd_id` int(11) NULL DEFAULT NULL COMMENT 'sys_curd表主键ID',
+  `create_table_sql` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '创建表的sql',
+  `field_set` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '字段配置',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for sys_menu
+-- ----------------------------
 DROP TABLE IF EXISTS `sys_menu`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `sys_menu` (
+CREATE TABLE `sys_menu`  (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
-  `name` varchar(255) DEFAULT NULL COMMENT '标题',
-  `rule` varchar(255) DEFAULT NULL COMMENT '规则',
-  `is_menu` tinyint(4) DEFAULT NULL COMMENT '菜单',
-  `sort` int(11) DEFAULT NULL COMMENT '排序',
-  `pid` int(11) DEFAULT NULL COMMENT '父级',
-  `icon` varchar(45) DEFAULT NULL COMMENT '图标',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '标题',
+  `rule` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '规则',
+  `is_menu` tinyint(4) NULL DEFAULT NULL COMMENT '菜单',
+  `sort` int(11) NULL DEFAULT NULL COMMENT '排序',
+  `pid` int(11) NULL DEFAULT NULL COMMENT '父级',
+  `icon` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '图标',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 22 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
---
--- Dumping data for table `sys_menu`
---
+-- ----------------------------
+-- Records of sys_menu
+-- ----------------------------
+INSERT INTO `sys_menu` VALUES (1, '首页', 'admin/index/index', 1, 0, 0, 'layui-icon layui-icon-home');
+INSERT INTO `sys_menu` VALUES (17, '系统核心', 'admin/core.menu/index', 1, 0, 0, 'layui-icon layui-icon-template-1');
+INSERT INTO `sys_menu` VALUES (18, '权限管理', 'admin/core.menu/index', 1, 0, 17, 'layui-icon layui-icon-align-left');
+INSERT INTO `sys_menu` VALUES (19, '菜单管理', 'admin/core.menu/index', 1, 0, 18, 'layui-icon layui-icon-templeate-1');
+INSERT INTO `sys_menu` VALUES (20, '一键生成', 'admin/autocreate.curd/index', 1, 0, 17, 'layui-icon layui-icon-share');
+INSERT INTO `sys_menu` VALUES (21, 'Curd', 'admin/autocreate.curd/index', 1, 0, 20, 'layui-icon layui-icon-star-fill');
 
-LOCK TABLES `sys_menu` WRITE;
-/*!40000 ALTER TABLE `sys_menu` DISABLE KEYS */;
-INSERT INTO `sys_menu` VALUES (1,'首页','/admin',1,0,0,'layui-icon layui-icon-home');
-/*!40000 ALTER TABLE `sys_menu` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2018-08-16 22:07:42
+SET FOREIGN_KEY_CHECKS = 1;
