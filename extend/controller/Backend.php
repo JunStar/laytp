eac5e3b60aad02ad220c2e3bacb4a669f8c92c5e
junstar <qmsjuncd@163.com>
2018-09-21 21:20:20 +0800

    重写tp基类的success和error方法，修改data和url参数的位置

--- extend/controller/Backend.php
+++ extend/controller/Backend.php
@@ -124,4 +124,14 @@ class Backend extends Controller
         }
         return $where;
     }
+
+    //重写tp基类的success方法，修改下data和url参数的位置
+    public function success($msg = '', $data = '', $url = null, $wait = 3, array $header = []){
+        return parent::success($msg, $url, $data, $wait, $header);
+    }
+
+    //重写tp基类的error方法，修改下data和url参数的位置
+    public function error($msg = '', $data = '', $url = null, $wait = 3, array $header = []){
+        return parent::error($msg, $url, $data, $wait, $header);
+    }
 }
