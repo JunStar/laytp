c5bbfb42449cbe7f2094f3bf90db49b4aae9bf29
junstar <qmsjuncd@163.com>
2018-10-19 16:20:36 +0800

    默认配置app.php应用类后缀和控制器后缀设置为false

--- config/demo/app.php
+++ config/demo/app.php
@@ -45,9 +45,9 @@ return [
     // 默认语言
     'default_lang'           => 'zh-cn',
     // 应用类库后缀
-    'class_suffix'           => true,
+    'class_suffix'           => false,
     // 控制器类后缀
-    'controller_suffix'      => true,
+    'controller_suffix'      => false,
 
     // +----------------------------------------------------------------------
     // | 模块设置
