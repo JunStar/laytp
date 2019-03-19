15a7b71235329fe7061debe1cf1e0cbc517f0fa0
junstar <qmsjuncd@163.com>
2019-03-19 11:16:05 +0800

    菜单管理

--- extend/library/Tree.php
+++ extend/library/Tree.php
@@ -26,8 +26,7 @@ class Tree
      * @var array
      */
     public $icon = array('│', '├', '└');
-//    public $nbsp = "&nbsp;";
-    public $nbsp = "_";
+    public $nbsp = "&nbsp;&nbsp;";
     public $pidname = 'pid';
 
     public function __construct($options = [])
