fd6465853552eac90564ba3930c8acbee9702932
junstar <qmsjuncd@163.com>
2019-04-01 10:24:27 +0800

    账号信息

--- public/static/admin/javascript/auth/user.js
+++ public/static/admin/javascript/auth/user.js
@@ -29,6 +29,7 @@ layui.use(['junAdmin'],function() {
 				,{field:'name',title:'姓名',align:'center'}
 				,{field:'avatar',title:'头像',align:'center'}
 				,{field:'is_super_manager',title:'是否为超管',align:'center'}
+				,{field:'status',title:'账号状态',align:'center'}
 				,{field:'create_time',title:'创建时间',align:'center'}
 				,{field:'operation',title:'操作',align:'center',toolbar:'#operation',fixed:'right',width:200}
             ]]
