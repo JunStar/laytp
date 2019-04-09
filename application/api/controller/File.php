68da4d303c76eee06aa9786cd8df440bdf6c3e69
junstar <qmsjuncd@163.com>
2019-04-09 10:06:30 +0800

    Api文档

--- application/api/controller/File.php
+++ application/api/controller/File.php
@@ -24,6 +24,12 @@ class File extends Api{
      */
     public function index()
     {
-        $this->success("返回成功", $this->request->request());
+        try{
+            $file = $this->request->file('file'); // 获取上传的文件
+            $info = $file->move('uploads'); // 移动文件到指定目录 没有则创建
+            $this->success('上传成功',['data'=>'/uploads/'.$info->getSaveName()]);
+        }catch (Exception $e){
+            $this->error('上传失败',['data'=>$e->getMessage()]);
+        }
     }
 }
--- extend/controller/Api.php
+++ extend/controller/Api.php
@@ -9,5 +9,13 @@ use think\Controller;
 
 class Api extends Controller
 {
+    //重写tp基类的success方法，修改下data和url参数的位置
+    public function success($msg = '', $data = '', $url = null, $wait = 3, array $header = []){
+        return parent::success($msg, $url, $data, $wait, $header);
+    }
 
+    //重写tp基类的error方法，修改下data和url参数的位置
+    public function error($msg = '', $data = '', $url = null, $wait = 3, array $header = []){
+        return parent::error($msg, $url, $data, $wait, $header);
+    }
 }
