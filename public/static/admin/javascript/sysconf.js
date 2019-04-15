f756d9c93ff2277971c7aadd945bae6436541693
junstar <qmsjuncd@163.com>
2019-04-15 16:17:36 +0800

    修改布局模板

--- public/static/admin/javascript/sysconf.js
+++ public/static/admin/javascript/sysconf.js
@@ -4,5 +4,20 @@ layui.use(['layTp'],function() {
         , $ = layui.jquery
     ;
 
+    //监听选择表下拉框onchange事件
+    layui.form.on('select(select_type)',function(data){
+        let show_content_types = ['select_single','select_multi','checkbox','radio','array'];
+        let upload_value_input = [];
+        if( show_content_types.indexOf(data.value) != -1 ){
+            $('#form-item-content').show();
+        }else{
+            $('#form-item-content').hide();
+        }
 
+        if( upload_value_input.indexOf(data.value) != -1 ){
+            $('#form-item-upload').show();
+        }else{
+            $('#form-item-upload').hide();
+        }
+    });
 });
