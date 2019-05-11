b2574179b9a71f4b3dc5e6dfde48749ae11792e9
junstar <qmsjuncd@163.com>
2019-05-11 11:53:58 +0800

    添加font-awesome

---
+++ application/admin/model/admin/Log.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * 管理员日志表模型
+ */
+namespace app\admin\model\admin;
+
+use think\Model;
+
+class Log extends Model
+{
+    //模型名
+    protected $name = 'admin_log';
+
+    //表名
+    
+
+    //数组常量
+    public $const = [
+
+    ];
+
+    //获取数组常量的函数
+    
+}
