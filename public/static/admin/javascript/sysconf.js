7c8cdd9434342679ab22aa193217743ab26edd7b
junstar <qmsjuncd@163.com>
2019-05-12 22:07:06 +0800

    系统配置增加删除功能

--- public/static/admin/javascript/sysconf.js
+++ public/static/admin/javascript/sysconf.js
@@ -28,4 +28,31 @@ layui.use(['layTp'],function() {
         let click_obj = $(this);
         click_obj.parent().parent().remove();
     });
+
+    $(document).on('click','.delete-item',function(){
+        let click_obj = $(this);
+        layui.layer.confirm('确定删除么?', function(index){
+            $.ajax({
+                type: 'POST',
+                url: layTp.facade.url('admin/sysconf/del'),
+                data: {id:click_obj.data('id')},
+                dataType: 'json',
+                success: function (res) {
+                    if( res.code == 1 ){
+                        click_obj.parent().parent().remove();
+                    }else{
+                        layTp.facade.error(res.msg);
+                    }
+                    layui.layer.close(index);
+                },
+                error: function (xhr) {
+                    if( xhr.status == '500' ){
+                        layTp.facade.error('本地网络问题或者服务器错误');
+                    }else if( xhr.status == '404' ){
+                        layTp.facade.error('请求地址不存在');
+                    }
+                }
+            });
+        });
+    });
 });
