d5886b144e98c95885ca0ad1bd0180f741cec879
junstar <qmsjuncd@163.com>
2019-12-13 22:31:24 +0800

    样式

--- application/admin/controller/autocreate/Curd.php
+++ application/admin/controller/autocreate/Curd.php
@@ -6,6 +6,7 @@ namespace app\admin\controller\autocreate;
 
 use controller\Backend;
 use think\Db;
+use think\Exception;
 use think\facade\Config;
 
 class Curd extends Backend
@@ -71,83 +72,87 @@ class Curd extends Backend
 
     //根据表名获取字段列表
     public function get_curd_info(){
-        $table = $this->request->param('table_name');
-        if(!$table){
-            $this->error('请选择表名',[]);
-        }
+        try{
+            $table = $this->request->param('table_name');
+            if(!$table){
+                $this->error('请选择表名',[]);
+            }
 
-        //查找是否已经生成过，生成过用生成过的数据渲染默认的详细设置，没有生成过使用数据库配置渲染详细设置
-        $curd_info = $this->model->where('table_name','=',$table)->find();
-        $model = Db::table($table);
-        $pk = $model->getPk();
-        $fields = $model->getTableFields();
-        $comment = model('InformationSchema')->getFieldsComment($table)->toArray();
-        $comment_map = arr_to_map($comment,'COLUMN_NAME');
-        //生成过用生成过的数据渲染默认的详细设置
-        if($curd_info){
-            $result['selected_list'] = json_decode($curd_info['field_list'], true);
-            $result['relation_model'] = json_decode($curd_info['relation_model'], true);
-            $result['global'] = json_decode($curd_info['global'], true);
-            if($result['relation_model']){
-                foreach($result['relation_model'] as $k=>$v){
-                    if(!isset($result['fields_list'][$v['table_name']])) {
-                        $temp_fields = Db::table($v['table_name'])->getTableFields($v['table_name']);
+            //查找是否已经生成过，生成过用生成过的数据渲染默认的详细设置，没有生成过使用数据库配置渲染详细设置
+            $curd_info = $this->model->where('table_name','=',$table)->find();
+            $model = Db::table($table);
+            $pk = $model->getPk();
+            $fields = $model->getTableFields();
+            $comment = model('InformationSchema')->getFieldsComment($table)->toArray();
+            $comment_map = arr_to_map($comment,'COLUMN_NAME');
+            //生成过用生成过的数据渲染默认的详细设置
+            if($curd_info){
+                $result['selected_list'] = json_decode($curd_info['field_list'], true);
+                $result['relation_model'] = json_decode($curd_info['relation_model'], true);
+                $result['global'] = json_decode($curd_info['global'], true);
+                if($result['relation_model']){
+                    foreach($result['relation_model'] as $k=>$v){
+                        if(!isset($result['fields_list'][$v['table_name']])) {
+                            $temp_fields = Db::table($v['table_name'])->getTableFields($v['table_name']);
 //                        $temp_pk = Db::table($v['table_name'])->getPk();
 //                        $result['fields_list'][$v['table_name']] = array_diff( $temp_fields, [$temp_pk] );
-                        $result['fields_list'][$v['table_name']] = $temp_fields;
+                            $result['fields_list'][$v['table_name']] = $temp_fields;
+                        }
                     }
                 }
-            }
-            $result['fields_list'][$table] = $fields;
-            foreach($result['selected_list'] as $k=>$v){
-                if($v['form_type'] == 'select_page'){
-                    if(!isset($result['fields_list'][$v['form_additional']['table_name']])){
-                        $result['fields_list'][$v['form_additional']['table_name']] = Db::table($v['form_additional']['table_name'])->getTableFields($v['form_additional']['table_name']);
+                $result['fields_list'][$table] = $fields;
+                foreach($result['selected_list'] as $k=>$v){
+                    if($v['form_type'] == 'select_page'){
+                        if(!isset($result['fields_list'][$v['form_additional']['table_name']])){
+                            $result['fields_list'][$v['form_additional']['table_name']] = Db::table($v['form_additional']['table_name'])->getTableFields($v['form_additional']['table_name']);
+                        }
                     }
                 }
-            }
-            $selected_list = arr_to_map(json_decode($curd_info['field_list'], true), 'field_name');
-            foreach($fields as $k=>$v){
-                if( !in_array($v, array_merge([$pk],$this->special_fields)) ) {
-                    if(isset($selected_list[$v])){
-                        $result['all_fields'][] = $selected_list[$v];
-                    }else{
-                        $result['all_fields'][] = [
-                            'field_name' => $v
-                            ,'field_comment' => $comment_map[$v]['COLUMN_COMMENT']
-                            ,'form_empty' => ( $comment_map[$v]['IS_NULLABLE'] == 'NO' ) ? 0 : 1
-                            ,'field_show_add' => 1
-                            ,'field_show_edit' => 1
-                            ,'field_show_index' => 1
-                        ];
+                $selected_list = arr_to_map(json_decode($curd_info['field_list'], true), 'field_name');
+                foreach($fields as $k=>$v){
+                    if( !in_array($v, array_merge([$pk],$this->special_fields)) ) {
+                        if(isset($selected_list[$v])){
+                            $result['all_fields'][] = $selected_list[$v];
+                        }else{
+                            $result['all_fields'][] = [
+                                'field_name' => $v
+                                ,'field_comment' => $comment_map[$v]['COLUMN_COMMENT']
+                                ,'form_empty' => ( $comment_map[$v]['IS_NULLABLE'] == 'NO' ) ? 0 : 1
+                                ,'field_show_add' => 1
+                                ,'field_show_edit' => 1
+                                ,'field_show_index' => 1
+                            ];
+                        }
                     }
                 }
-            }
-            $this->success('获取成功', $result);
-            //没有生成过使用数据库配置渲染详细设置
-        }else{
-            $result = [];
-            $i = 0;
-            foreach($fields as $k=>$v){
-                if( $v != $pk ){
-                    $result['all_fields'][$i]['field_name'] = $v;
-                    $result['all_fields'][$i]['field_comment'] = $comment_map[$v]['COLUMN_COMMENT'];
-                    $result['all_fields'][$i]['form_empty'] = ( $comment_map[$v]['IS_NULLABLE'] == 'NO' ) ? 0 : 1;
-                    $result['all_fields'][$i]['field_show_index'] = 1;
-                    $result['all_fields'][$i]['field_show_add'] = 1;
-                    $result['all_fields'][$i]['field_show_edit'] = 1;
-                    if($v != 'delete_time' && $v != 'update_time'){
-                        $result['selected_list'][$i]['field_name'] = $v;
-                        $result['selected_list'][$i]['field_comment'] = $comment_map[$v]['COLUMN_COMMENT'];
-                        $result['selected_list'][$i]['field_show_index'] = 1;
-                        $result['selected_list'][$i]['field_show_add'] = 1;
-                        $result['selected_list'][$i]['field_show_edit'] = 1;
+                $this->success('获取成功', $result);
+                //没有生成过使用数据库配置渲染详细设置
+            }else{
+                $result = [];
+                $i = 0;
+                foreach($fields as $k=>$v){
+                    if( $v != $pk ){
+                        $result['all_fields'][$i]['field_name'] = $v;
+                        $result['all_fields'][$i]['field_comment'] = $comment_map[$v]['COLUMN_COMMENT'];
+                        $result['all_fields'][$i]['form_empty'] = ( $comment_map[$v]['IS_NULLABLE'] == 'NO' ) ? 0 : 1;
+                        $result['all_fields'][$i]['field_show_index'] = 1;
+                        $result['all_fields'][$i]['field_show_add'] = 1;
+                        $result['all_fields'][$i]['field_show_edit'] = 1;
+                        if($v != 'delete_time' && $v != 'update_time'){
+                            $result['selected_list'][$i]['field_name'] = $v;
+                            $result['selected_list'][$i]['field_comment'] = $comment_map[$v]['COLUMN_COMMENT'];
+                            $result['selected_list'][$i]['field_show_index'] = 1;
+                            $result['selected_list'][$i]['field_show_add'] = 1;
+                            $result['selected_list'][$i]['field_show_edit'] = 1;
+                        }
+                        $i++;
                     }
-                    $i++;
                 }
+                $result['fields_list'][$table] = $fields;
+                $this->success('获取成功', $result);
             }
-            $result['fields_list'][$table] = $fields;
-            $this->success('获取成功', $result);
+        }catch (Exception $e){
+            $this->error($e->getMessage());
         }
     }
 
