f74856c7a7971062ad329b64fafd554382048f14
junstar <qmsjuncd@163.com>
2020-05-07 14:57:50 +0800

    插件机制

--- route/route.php
+++ route/route.php
@@ -3,6 +3,4 @@ use \think\facade\Route;
 
 // 定义laytp插件路由规则
 Route::any('addons/:addon/', "\\library\\AddonsRoute@execute");
-Route::domain('autocreate', function(){
-
-}, ['addon'=>'autocreate']);
+Route::domain('autocreate', "\\library\\AddonsRoute@execute", ['addon'=>'autocreate']);
