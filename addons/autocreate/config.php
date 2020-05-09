b55ce69821e14cb898f153561f544c7e8a0eb804
junstar <qmsjuncd@163.com>
2020-05-09 17:25:53 +0800

    插件机制

--- addons/autocreate/config.php
+++ addons/autocreate/config.php
@@ -1,4 +1,13 @@
 <?php
 return [
-
+    [
+        'name'=>'特殊字段，自动生成不需要详细设置的字段'
+        ,'key'=>'special_fields'
+        ,'type'=>'array'
+    ],
+    [
+        'name'=>'不能使用curd生成的系统表名'
+        ,'key'=>'system_tables'
+        ,'type'=>'array'
+    ]
 ];
