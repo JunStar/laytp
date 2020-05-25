6d270d68bfc3c2b2bd83768310606a5250b492ea
junstar <qmsjuncd@163.com>
2020-05-25 08:36:06 +0800

    主题样式

--- application/common/model/User.php
+++ application/common/model/User.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * 前台用户管理模型
+ * 用户表模型
  */
 namespace app\common\model;
 
@@ -9,9 +9,39 @@ use think\model\concern\SoftDelete;
 
 class User extends Backend
 {
-	use SoftDelete;
+    use SoftDelete;
     //模型名
     protected $name = 'user';
 
+    //时间戳字段转换
+
+    //是否设置创建时间字段，当设置$createTime = false时，为关闭create_time自动写入，默认值为$createTime = 'create_time'
+
+    //是否设置更新时间字段，当设置$updateTime = false时，为关闭update_time自动写入，默认值为$updateTime = 'update_time'
+
+    //是否设置删除时间字段，当设置$deleteTime = false时，为关闭delete_time自动写入，默认值为$deleteTime = 'delete_time'
+
+
+    //表名
+
+
+    //数组常量
+    public $const = [
+        'status' => [
+            '1'=>'正常'
+            ,'2'=>'锁定'
+        ],
+    ];
+
     //关联模型
+
+
+    //获取数组常量的函数
+    public function getArrayConstList($field_name){
+        return $this->const[$field_name];
+    }
+
+    public function getAvatarAttr($avatar){
+        return $avatar ? : '/static/index/image/default.png';
+    }
 }
