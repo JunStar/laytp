42d835052fd0f1ce22452539346465c76a207f69
junstar <qmsjuncd@163.com>
2020-06-04 15:23:50 +0800

    插件

---
+++ addons/aliyuncs/service/Oss.php
@@ -0,0 +1,50 @@
+<?php
+namespace addons\aliyuncs\service;
+
+use service\Service;
+
+class Oss extends Service
+{
+    protected static $instance;
+
+    /**
+     * 单例
+     * @return static
+     */
+    public static function instance()
+    {
+        if (is_null(self::$instance)) {
+            self::$instance = new static();
+        }
+
+        return self::$instance;
+    }
+
+    /**
+     * 上传文件
+     * @param $local_file_name
+     * @param $save_file_name
+     * @return bool
+     * @throws Exception
+     */
+    public function upload($local_file_name, $save_file_name){
+        try{
+            $access_key = Config::get('addons.qiniu.access_key');
+            $secret_key = Config::get('addons.qiniu.secret_key');
+            $bucket = Config::get('addons.qiniu.bucket');
+            $client = new Auth($access_key,$secret_key);
+            $token = $client->uploadToken($bucket);
+            $upload_mgr = new UploadManager();
+            list($ret, $err) = $upload_mgr->putFile($token,$save_file_name,$local_file_name);
+            if ($err !== null) {
+                $this->setError('上传失败,'.$err);
+                return false;
+            } else {
+                return true;
+            }
+        }catch (\Exception $e){
+            $this->setError('上传失败,'.$e->getMessage());
+            return false;
+        }
+    }
+}
--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -14,7 +14,6 @@ return array(
     'Stringy\\' => array($vendorDir . '/danielstjules/stringy/src'),
     'Psr\\Http\\Message\\' => array($vendorDir . '/psr/http-message/src'),
     'PHPMailer\\PHPMailer\\' => array($vendorDir . '/phpmailer/phpmailer/src'),
-    'OSS\\' => array($vendorDir . '/aliyuncs/oss-sdk-php/src/OSS'),
     'JmesPath\\' => array($vendorDir . '/mtdowling/jmespath.php/src'),
     'GuzzleHttp\\Psr7\\' => array($vendorDir . '/guzzlehttp/psr7/src'),
     'GuzzleHttp\\Promise\\' => array($vendorDir . '/guzzlehttp/promises/src'),
