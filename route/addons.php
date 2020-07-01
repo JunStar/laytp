9513fe230e7902dec8151f33a60904318e53a7f7
junstar <qmsjuncd@163.com>
2020-07-01 12:19:46 +0800

    插件

--- route/addons.php
+++ route/addons.php
@@ -7,18 +7,19 @@ use think\facade\Route;
 Route::any('/addons/:addon/', "\\library\\AddonsRoute@execute");
 
 //Route::domain('*', function(){
-//    //正常插件访问路由，例：http://www.yourdomain.com/addons/demo/[module]/[controller]/[action]
-//
-//
 //    //绑定了二级域名插件访问路由，例：http://demo.yourdomain.com/[module]/[controller]/[action]
 //    $request = Request::instance();
 //    $host = $request->host();
 //    $addon_service = new \app\admin\service\Addons();
 //    $addon = $addon_service->_info->getAddonByDomain($host);
-//
-//    $addon_service = new \app\admin\service\Addons();
-//    $addon_info = $addon_service->_info->getAddonInfo($addon);
-//    if($addon_info){
-//        exit(\library\AddonsRoute::domain_execute($addon));
+//    if($addon){
+//        $addon_service = new \app\admin\service\Addons();
+//        $addon_info = $addon_service->_info->getAddonInfo($addon);
+//        if($addon_info){
+//            exit(\library\AddonsRoute::domain_execute($addon));
+//        }
 //    }
 //});
+
+//正常插件访问路由，例：http://www.yourdomain.com/addons/demo/[module]/[controller]/[action]
+//Route::any('/addons/:addon/', "\\library\\AddonsRoute@execute");
