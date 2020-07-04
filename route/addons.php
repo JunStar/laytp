5817e5b9f1ef98eb1b1b1833c3a442f2cd5b5855
junstar <qmsjuncd@163.com>
2020-07-04 15:28:33 +0800

    插件

--- route/addons.php
+++ route/addons.php
@@ -6,11 +6,13 @@ use think\facade\Route;
 Route::any('/addons/:addon/', "\\library\\AddonsRoute@execute");
 
 $addons_domains = \think\facade\Config::get('addons.domains');
-$host = \think\facade\Request::server('HTTP_HOST');
-if(in_array($host,$addons_domains)){
-    Route::domain('autocreate.laytp.com', function(){
-        $flip_addons_domains = array_flip(\think\facade\Config::get('addons.domains'));
-        $host = \think\facade\Request::server('HTTP_HOST');
-        exit(\library\AddonsRoute::domain_execute($flip_addons_domains[$host]));
-    });
+if($addons_domains){
+    $host = \think\facade\Request::server('HTTP_HOST');
+    if(in_array($host,$addons_domains)){
+        Route::domain('autocreate.laytp.com', function(){
+            $flip_addons_domains = array_flip(\think\facade\Config::get('addons.domains'));
+            $host = \think\facade\Request::server('HTTP_HOST');
+            exit(\library\AddonsRoute::domain_execute($flip_addons_domains[$host]));
+        });
+    }
 }
