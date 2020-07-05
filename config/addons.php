e16323435fac03cfe4308bcb84215e160cd25e3c
JunStar <qmsjuncd@163.com>
2020-07-05 15:21:36 +0800

    Default Changelist

--- config/addons.php
+++ config/addons.php
@@ -1,38 +1,4 @@
 <?php
 return array (
-  'api_url' => 'http://www.laytp.com',
-  'autocreate' => 
-  array (
-    'special_fields' => 
-    array (
-      0 => 'id',
-    ),
-    'system_tables' => 
-    array (
-      0 => 'lt_admin_log',
-      1 => 'lt_admin_menu',
-      2 => 'lt_admin_role',
-      3 => 'lt_admin_role_rel_menu',
-      4 => 'lt_admin_role_rel_user',
-      5 => 'lt_admin_user',
-      6 => 'lt_autocreate_curd',
-      7 => 'lt_sysconf',
-      8 => 'lt_autocreate_menu',
-      9 => 'lt_user_token',
-      10 => 'lt_area',
-      11 => 'lt_attachment',
-      12 => 'lt_user',
-    ),
-  ),
-  'editor' => 
-  array (
-    0 => 'ueditor',
-    1 => 'mdeditor',
-  ),
-  'ueditor' => 
-  array (
-  ),
-  'mdeditor' => 
-  array (
-  ),
+
 );
