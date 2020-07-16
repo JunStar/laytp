85a23c2f78e835f52bf74aa554871e5bc8c2802c
JunStar <qmsjuncd@163.com>
2020-07-17 00:31:55 +0800

    新登录界面

--- application/admin/controller/auth/Login.php
+++ application/admin/controller/auth/Login.php
@@ -44,6 +44,39 @@ class Login extends Controller
         return $this->fetch();
     }
 
+    //登录界面
+    public function index_old(){
+        $token = $this->request->server('HTTP_ADMIN_TOKEN', $this->request->request('admin_token', Cookie::get('admin_token')));
+        if( $token ){
+            $data = Token::get($token);
+            if(isset($data['user_id'])){
+                return $this->redirect(url('/admin?ref=1'));
+            }
+        }
+        $referer = $this->request->server('HTTP_REFERER');
+        $host = $this->request->server('HTTP_HOST');
+        if(
+            "http://".$host.'/' == $referer || "https://".$host.'/' == $referer ||
+            "http://".$host.'/install.php/' == $referer || "https://".$host.'/install.php/' == $referer
+        ){
+            $referer = $referer.'admin';
+        }
+        $normal_logout = Cookie::get('normal_logout');
+        if(!$normal_logout && $referer){
+            $parse_url = parse_url($referer);
+            $query = '';
+            if(isset($parse_url['query'])){
+                $query = str_replace('=','/',$parse_url['query']);
+                $query = str_replace('ref','laytp_menu_id',$query);
+            }
+            $this->assign('referer',$parse_url['scheme'].'://'.$parse_url['host'].$parse_url['path'].'/'.$query);
+        }else{
+            $this->assign('referer','');
+        }
+        Cookie::set('normal_logout',0);
+        return $this->fetch();
+    }
+
     //登录
     public function do_login(){
         $validate = new \app\admin\validate\auth\Login;
