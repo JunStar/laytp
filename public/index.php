aae0502565c3d3ae3b913296e4af1ca3abe29453
junstar <qmsjuncd@163.com>
2020-08-06 10:04:28 +0800

    允许跨域

--- public/index.php
+++ public/index.php
@@ -1,6 +1,11 @@
 <?php
 namespace think;
 
+//允许跨域
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: token, Origin, X-Requested-With, Content-Type, Accept");
+header('Access-Control-Allow-Methods: POST,GET');
+
 define('APP_PATH', __DIR__ . '/../application/');
 
 // 判断是否安装LayTp
