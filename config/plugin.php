63fbe9fc516b2b945bc114b40d818098e6bc5822
JunStar <qmsjuncd@163.com>
2022-01-18 21:06:51 +0800

    修改插件安装配置文件，回归默认

--- config/plugin.php
+++ config/plugin.php
@@ -3,12 +3,7 @@ return array (
   'apiUrl' => 'https://api.laytp.com',
   'installed' => 
   array (
-    0 => 'ali_sms',
-    1 => 'apidoc',
-    2 => 'curd',
-    3 => 'email',
-    4 => 'meditor',
-    5 => 'ueditor',
+
   ),
   'installedEditor' => 
   array (
