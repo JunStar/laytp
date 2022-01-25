41553d26df8cbaec662afc35ef0cf67bfd61f807
JunStar <qmsjuncd@163.com>
2022-01-25 23:02:40 +0800

    【优化】插件市场，新增一列，最新版本

--- app/service/admin/PluginsServiceFacade.php
+++ app/service/admin/PluginsServiceFacade.php
@@ -11,6 +11,7 @@ use think\Facade;
  * @method static mixed offLineInstall() 离线安装
  * @method static mixed unInstall($plugin) 卸载
  * @method static mixed getPluginPath($plugin) 获取插件目录
+ * @method static mixed getPluginInfo($plugin) 获取插件信息
  * @method static mixed install($plugin, $laytpGwToken) 安装插件
  */
 class PluginsServiceFacade extends Facade
